33511376f1a8ba08649b3066a44cfee5acbeaa79
Eryan GAUVRIT <eryan.gauvrit@gmail.com>
2024-11-15 16:20:01 +0100

    Project init

---
+++ src/main/java/com/p3openclassrooms/p3oc/P3ocApplication.java
@@ -0,0 +1,13 @@
+package com.p3openclassrooms.p3oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3ocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p3openclassrooms/p3oc/P3ocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p3openclassrooms.p3oc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
